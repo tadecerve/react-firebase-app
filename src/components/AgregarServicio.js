4dc6f6e2070839db9314a9733ad7ca52c966e272
tadecerve <tcervellati123@gmail.com>
2023-09-29 19:26:36 -0300

    a

--- src/components/AgregarServicio.js
+++ src/components/AgregarServicio.js
@@ -118,16 +118,61 @@ export function AgregarServicio() {
             </div>
 
             <div className="campo">
-            <input
-              type="submit"
-              value="Enviar"
-              className="boton boton--primario"
-            />
-          </div>
+              <input
+                type="submit"
+                value="Enviar"
+                className="boton boton--primario"
+              />
+            </div>
           </form>
         </main>
-
-
+        <ul
+          className="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm"
+          id="pillNav2"
+          role="tablist"
+          style={{
+            "--bs-nav-link-color": "var(--bs-white)",
+            "--bs-nav-pills-link-active-color": "var(--bs-primary)",
+            "--bs-nav-pills-link-active-bg": "var(--bs-white)",
+          }}
+        >
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active rounded-5"
+              id="home-tab2"
+              data-bs-toggle="tab"
+              type="button"
+              role="tab"
+              aria-selected="true"
+            >
+              Home
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link rounded-5"
+              id="profile-tab2"
+              data-bs-toggle="tab"
+              type="button"
+              role="tab"
+              aria-selected="false"
+            >
+              Profile
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link rounded-5"
+              id="contact-tab2"
+              data-bs-toggle="tab"
+              type="button"
+              role="tab"
+              aria-selected="false"
+            >
+              Contact
+            </button>
+          </li>
+        </ul>
 
         {/* FOOTER */}
         <footer className="footer">
