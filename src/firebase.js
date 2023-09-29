8a59f82634de59d213824512129e670b607cfb4f
tadecerve <tcervellati123@gmail.com>
2023-09-29 19:04:36 -0300

    act

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  getBytes,
+} from "firebase/storage";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  query,
+  where,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,9 +30,12 @@ const firebaseConfig = {
   projectId: "react-fb-auth-f8395",
   storageBucket: "react-fb-auth-f8395.appspot.com",
   messagingSenderId: "598602805286",
-  appId: "1:598602805286:web:49023deb261521c72876e7"
+  appId: "1:598602805286:web:49023deb261521c72876e7",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
